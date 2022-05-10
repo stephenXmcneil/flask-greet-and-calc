8bf9b431cc9003aa224e64cabba79b445317aa03
stephenxmcneil <stephenXmcneil@gmail.com>
2022-05-10 08:00:56 -0400

    stephen's first commit

---
+++ greet/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/welcome')
+def say_hello():
+    # greet = """
+    # html>
+    #     <body>
+    #         <h1>Welcome</h1>
+    #     </body>
+    # </html>
+    # """
+    return "Welcome"
+
+@app.route('/welcome/home')
+def welcome_home():
+    return "Welcome Home"
+
+@app.route('/welcome/back')
+def welcome_back():
+    return "Welcome Back"
